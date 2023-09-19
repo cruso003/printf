13b8bb14b79604999ba5fff1d728292276bd338b
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-09-19 11:24:18 -0700

    added macros for char and strings

--- main.h
+++ main.h
@@ -6,6 +6,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Macro: WRITE_CHAR(c, count)
+ * Description: Writes a single char 'c' to stdout & increments d 'count' variable
+ * Parameters:
+ * c - The character to write.
+ *   count - A pointer to the count of characters written.
+ * Usage:
+ *   WRITE_CHAR('A', char_count);
+ */
+#define WRITE_CHAR(c, count) do { _putchar(c); (*(count))++; } while (0)
+
+/* 
+ * Macro: WRITE_STRING(s, len, count)
+ * Description: Writes a string 's' of length 'len' to stdout and updates the 'count' variable.
+ * Parameters:
+ *   s - The string to write.
+ *   len - The length of the string.
+ *   count - A pointer to the count of characters written.
+ * Usage:
+ *   char *message = "Hello, World!";
+ *   int length = strlen(message);
+ *   WRITE_STRING(message, length, char_count);
+ */
+#define WRITE_STRING(s, len, count) do { write(1, s, len); (*(count)) += len; } while (0)
+
 #define BUFFER_SIZE 1024
 
 int _putchar(char c);
