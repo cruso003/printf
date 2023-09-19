e611c404a7a458f4f09daee8dd61dece5bcc3c96
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-09-19 08:37:48 -0700

    created main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+int _putchar(char c);
+int _strlen(const char *str);
+int handle_percent(void);
+int _printf(const char *format, ...);
+int handle_format_specifier(const char format_char, va_list args, int *count);
+int handle_string(va_list args);
+int handle_char(va_list args);
+int handle_int(va_list args, int *count);
+int handle_bin(va_list args, int *count);
+int handle_unsigned_int(va_list args, int *count);
+int handle_octal(va_list args, int *count);
+int handle_hexadecimal(va_list args, int *count, int uppercase);
+int handle_non_printable(va_list args, int *count);
+int handle_addr(va_list args, int *count);
+
+#endif
