7d46013669480a0e6cdc4048b8d7da1f581538c9
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-19 18:49:00 +0300

    added custom _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @str: A pointer to type char.
+ * Return: len (short for lenght).
+ */
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
